9cb8e5d1cb3d680984c8477162a1126cd9242e8f
gauti123456 <geekygautam1997@gmail.com>
2023-11-03 15:31:48 +0530

    latest commit:

---
+++ app/users/page.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const UsersPage = () => {
+  return (
+    <div>
+        <h1>This is the users page</h1>
+        <Link href="/">Go to Home</Link>
+    </div>
+  )
+}
+
+export default UsersPage
